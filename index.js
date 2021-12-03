27960d0ae4d40226cb3482cd6ba3375ce1d85480
Santeri <Seppl>
2021-12-03 10:46:36 +0200

    testing database connection

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ app.use(cors());
 
 const port = process.env.PORT || 8080;
 
-const db = [{ name: "tiina" }, { name: "jack" }];
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
 
-app.get("/names", (req, res) => {
-  res.send(db);
-});
-
-app.get("/names", (req, res) => {
-  res.send(db);
+var pool = mysql.createPool(config);
+app.get("/", (req, res) => {
+  pool.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
