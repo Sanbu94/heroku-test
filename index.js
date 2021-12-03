3af653783258c6a2363cff58b4f6a6cda7872349
Santeri <Seppl>
2021-12-03 10:44:04 +0200

    fix typo

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const mysql = require("mysql");
 var cors = require("cors");
 app.use(cors());
 
+const port = process.env.PORT || 8080;
+
+const db = [{ name: "tiina" }, { name: "jack" }];
+
 app.get("/names", (req, res) => {
   res.send(db);
 });
 
-const port = process.env.PORT || 8080;
+app.get("/names", (req, res) => {
+  res.send(db);
+});
+
 const server = app.listen(port, () => {
   console.log(`Listening on port ${server.address().port}`);
 });
